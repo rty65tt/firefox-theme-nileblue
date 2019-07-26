ab576cacbf6fe621fe3b6ddef4b7db238558e7e3
u1 <rty65tt@gmail.com>
2019-07-27 00:40:34 +0300

    Remove accentcolor, textcolor, and headerURL option

--- src/setsettingstheme.js
+++ src/setsettingstheme.js
@@ -1,37 +1,38 @@
 
 var default_theme =  {
     "colors": {
-                "accentcolor": "#49627a",
 
-                "frame": "#283858",
+                "frame": "#49627a",
                 "frame_inactive": "#17314A", 
                 
-                "button_background_hover": "#49627a",
-                "button_background_active": "#19324d",
-
                 "icons": "#000000",
-                //"icons_attention": "red",
+                "icons_attention": "#000000",
+                "button_background_hover": "#19324D",
+                "button_background_active": "#19324D",
+                "bookmark_text": "#000000",
 
                 "popup": "#819da3",
+                "popup_border": "#819da3",
                 "popup_text": "#000000",
                 "popup_highlight": "#49627a",
                 "popup_highlight_text": "#00000",
 
                 "tab_text": "#A7ABC7",
+                "tab_background_text": "#25313F",
                 "tab_line": "#19324d",
                 "tab_selected": "#19324d",
                 "tab_loading": "#94814c",
                 
                 "tab_background_separator": "#19324d",
 
-                "textcolor": "#25313F",
-
                 "toolbar": "#39526d",
                 "toolbar_text": "#000000",
                 "toolbar_field": "#213d53",
-                "toolbar_field_focus": "#19324d",
                 "toolbar_field_text": "#91bdc3",
+                "toolbar_field_focus": "#19324d",
                 "toolbar_field_text_focus": "#91bdc3",
+                "toolbar_field_highlight": "#819DA3",
+                "toolbar_field_highlight_text": "#000000",
                 "toolbar_field_border": "#213d53",
                 "toolbar_field_border_focus": "#19324d",
                 "toolbar_field_separator": "#39526d",
@@ -42,14 +43,13 @@ var default_theme =  {
                 "ntp_background": "#1a3344",
                 "ntp_text": "#6a6e71",
 
-
-                "sidebar": "#39546a",
+                "sidebar": "#819DA3",
+                "sidebar_border": "#819DA3",
                 "sidebar_text": "#000000",
                 "sidebar_separator": "#000000",
 
                 "sidebar_highlight": "#314D63",
                 "sidebar_highlight_text": "#000000"
-
             }
     }
 
